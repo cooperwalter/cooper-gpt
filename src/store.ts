043f284203a0c2801c56e99bde0c3b8395ee4b28
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2023-04-26 18:18:38 -0700

    persist chat state on reload

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { useDispatch } from 'react-redux'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import chatsReducer from './chats/chatsSlice'
 
-const store = configureStore({
-  reducer: {
-    chats: chatsReducer
-  }
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
+  chats: chatsReducer
 })
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }),
+});
+
+const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export const useAppDispatch: () => AppDispatch = useDispatch // Export a hook that can be reused to resolve types
 
-export default store
+export { store, persistor }
