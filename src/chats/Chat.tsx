f0d83b0027b1b4127be4232b212dfe168e8c6c7d
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2023-04-26 19:30:43 -0700

    add empty placeholder for chat

--- src/chats/Chat.tsx
+++ src/chats/Chat.tsx
@@ -9,13 +9,13 @@ const Chat = (props: ChatProps) => {
   const messages = useChats(chatId)
   return (
     <div className="chat">
-      {messages.map((message, index) => {
+      {messages.length > 0 ? messages.map((message, index) => {
         console.log(`chat_message chat_message-${message.role}`)
         return (
         <div key={index} className={`chat_message chat_message-${message.role}`}>
           {message.role === 'user' ? 'You: ' : 'Cooper: '}{message.content}
         </div>
-      )})}
+      )}) : <div className="chat__empty"><h2 className="chat__empty__text">CooperChat</h2></div>}
     </div>
   )
 }
