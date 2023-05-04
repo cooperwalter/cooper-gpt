4c55ee73b8deaa93c23b81617f2ea773e5d031ef
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2023-05-03 17:43:13 -0700

    add development and production api urls

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,30 @@
-const config = {
+const defaultConfig = {
   api: {
-    url: 'http://localhost:8000'
+    url: ''
+  }
+}
+
+let config
+
+switch(import.meta.env.MODE) {
+  case 'development': {
+    config = {
+      ...defaultConfig,
+      api: {
+        ...defaultConfig.api,
+        url: 'http://localhost:8000'
+      }
+    }
+    break
+  } 
+  default: {
+    config = {
+      ...defaultConfig,
+      api: {
+        ...defaultConfig.api,
+        url: 'https://api-coopergpt.netlify.app'
+      }
+    }
   }
 }
 
